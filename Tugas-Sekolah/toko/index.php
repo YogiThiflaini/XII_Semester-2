fcda6434a1f9d4ff69312876eff6847dc84c11a1
YogiThiflaini <140322684+YogiThiflaini@users.noreply.github.com>
2024-01-26 10:04:04 +0700

    tgl 26-01-24

--- Tugas-Sekolah/toko/index.php
+++ Tugas-Sekolah/toko/index.php
@@ -1,13 +1,33 @@
-<?php 
-$host="127.0.0.1";
-$user="root";
-$password="";
-$db="dbtoko";
+<form action="" method="post">
+    barang:
+    <input type="text" name="namabarang" placeholder="nama barang" >
+    <br>
+    harga:
+    <input type="number" name="harga" placeholder="harga barang">
+    <br>
+    stok:
+    <input type="number" name="stok" placeholder="stok barang">
+    <br>
+    <input type="submit" name="simpan" value="simpan">
+</form>
 
-$koneksi=new mysqli($host,$user,$password,$db);
+<?php 
+ $host="127.0.0.1";
+ $user="root";
+ $password="";
+ $db="dbtoko";
+ 
+ $koneksi=new mysqli($host, $user, $password, $db);
+
+if(isset($_POST["simpan"])){
+    $namabarang=$_POST["namabarang"];
+    $harga=$_POST["harga"];
+    $stock=$_POST["stok"];
+$sql="INSERT INTO barang (namabarang,harga,stock) VALUES ('$namabarang',$harga,$stock)";
+$hasil=mysqli_query($koneksi, $sql);
+}
 
 $sql="SELECT * FROM barang";
-
 $hasil=mysqli_query($koneksi, $sql);
 
 var_dump($hasil);
@@ -29,53 +49,15 @@ echo "<table border=2px>
         <tbody>";
 
 while($row=mysqli_fetch_array($hasil)){
-    echo"<tr>";
-        echo "<td>". $row[1]."</td>";
-        echo "<td>". $row[2]."</td>";
-        echo "<td>". $row[3]."</td>";
-    echo"</tr>";
+    echo "<tr>";
+        echo "<td> " . $row[1] . "</td>";
+        echo "<td> " . $row[2] . "</td>";
+        echo "<td> " . $row[3] . "</td>";
+    echo "</tr>";
 }
 
-echo "</tbody>
+echo "  </tbody>
     </table>";
 
-echo "<br><br>";
-
-//pelanggan
-
-$sql="SELECT * FROM pelanggan";
-$hasil=mysqli_query($koneksi, $sql);
-
-var_dump($hasil);
-
-echo "<table border=2px>
-        <thead>
-            <tr>
-                <th>
-                    NO
-                </th>
-                <th>
-                    PELANGGAN
-                </th>
-                <th>
-                    ALAMAT
-                </th>
-                <th>
-                    TELEPON
-                </th>
-            </tr>
-        </thead>
-        <tbody>";
-
-while($row=mysqli_fetch_array($hasil)){
-    echo"<tr>";
-        echo "<td>". $row[0]."</td>";
-        echo "<td>". $row[1]."</td>";
-        echo "<td>". $row[2]."</td>";
-        echo "<td>". $row[3]."</td>";
-    echo"</tr>";
-}
-
-echo "</tbody>
-    </table>";      
+echo "<br><br>";      
 ?>
