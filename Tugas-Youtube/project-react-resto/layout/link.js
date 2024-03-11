e41b8af03bd32378a68c8eba817a87a3f912128d
YogiThiflaini <140322684+YogiThiflaini@users.noreply.github.com>
2024-03-11 10:20:03 +0700

    react-js vid-18

---
+++ Tugas-Youtube/project-react-resto/layout/link.js
@@ -0,0 +1,9 @@
+const url = "http://localhost:8000/api";
+let token = "zkBG93HlR8dG0UAgl9dopjBSuqVd5oFUduGf6xb8";
+
+export const link = axios.create({
+    baseURL: url,
+    headers: {
+        'api_token': token
+    }
+});
